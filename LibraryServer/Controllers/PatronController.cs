17b48fd4459acd92ff461d91640231196cf84a55
TurbanMan <waleed.nadeem.309@my.csun.edu>
2024-03-22 18:36:40 -0700

    Added PatronController.cs

---
+++ LibraryServer/Controllers/PatronController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using LibraryServer.LibraryModel;
+
+namespace LibraryServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PatronController(LibraryGoldenContext context) : ControllerBase
+    {
+        // GET: api/Patron
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Patron>>> GetPatron()
+        {
+            return await context.Patrons.ToListAsync();
+        }
+    }
+}
